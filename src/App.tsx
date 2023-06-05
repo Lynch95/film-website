d833722e94c53a01ccedd1b0078d68c752082483
Lynch95 <barrylynch06@hotmail.com>
2023-06-05 17:59:07 +0100

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import ExpensesTable from "./components/ExpensesTable";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "gold", amount: 20, category: "metals" },
+    { id: 2, description: "silver", amount: 10, category: "metals" },
+    { id: 3, description: "bronze", amount: 5, category: "metals" },
+    { id: 4, description: "platinum", amount: 50, category: "metals" },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ExpensesTable expenses={expenses} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ExpensesTable.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenses: Expense[];
+}
+
+const ExpensesTable = ({ expenses }: Props) => {
+  return (
+    <table className='table table-bordered'>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ExpensesTable;
