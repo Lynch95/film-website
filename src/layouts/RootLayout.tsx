7205cbaba86726b15648d8c27cac7f9448634405
Lynch95 <barrylynch06@hotmail.com>
2023-06-08 23:35:51 +0100

    ok

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,27 @@
-import { Outlet } from "react-router-dom"
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import HeroImage from "../components/HeroImage";
+import NavBar from "../components/NavBar";
 
 export default function RootLayout() {
   return (
     <div>
-      <Outlet />
+      <Grid
+        templateAreas={`"nav nav"
+            "content content"
+            "footer footer"`}>
+        <GridItem pl='2' area={"nav"}>
+          <NavBar />
+        </GridItem>
+        <GridItem pl='2' area={"content"}>
+          <HeroImage />
+        </GridItem>
+        <GridItem pl='2' bg='blue.300' area={"footer"}>
+          footer
+        </GridItem>
+
+        <Outlet />
+      </Grid>
     </div>
-  )
+  );
 }
