79fc66a405d46db4118d6296e6f65e80b1473eae
Lynch95 <barrylynch06@hotmail.com>
2023-06-18 16:06:44 +0100

    work

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -1,4 +1,5 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 interface Film {
   id: number;
@@ -15,23 +16,45 @@ interface Props {
 }
 
 const Reviews = ({ films, onReview }: Props) => {
+  const [selectedFilm, setSelectedFilm] = useState<number | null>(null);
+
+  const handleClick = (film: Film) => {
+    onReview(film.id);
+    setSelectedFilm(film.id);
+  };
+
+  const getFilmMessage = (filmId: number) => {
+    switch (filmId) {
+      case 1:
+        return "Hello world";
+      case 3:
+        return "Good morning";
+      // Add more cases for other filmIds
+      default:
+        return "";
+    }
+  };
+
   return (
-    <SimpleGrid columns={{ base: 2, md: 4 }} spacing={10}>
-      {films.map((film) => (
-        <Box
-          key={film.id}
-          onClick={() => onReview(film.id)}
-          bgImage={film.bgImage}
-          bg={film.bgColor}
-          h={film.height}
-          border={film.border}
-          backgroundSize='contain'
-          backgroundRepeat='no-repeat'
-          backgroundPosition='center'>
-          {film.name}
-        </Box>
-      ))}
-    </SimpleGrid>
+    <>
+      <SimpleGrid columns={{ base: 2, md: 4 }} spacing={10}>
+        {films.map((film) => (
+          <Box
+            key={film.id}
+            onClick={() => handleClick(film)}
+            bgImage={film.bgImage}
+            bg={film.bgColor}
+            h={film.height}
+            border={film.border}
+            backgroundSize='contain'
+            backgroundRepeat='no-repeat'
+            backgroundPosition='center'>
+            {film.name}
+          </Box>
+        ))}
+      </SimpleGrid>
+      {selectedFilm && <Text>{getFilmMessage(selectedFilm)}</Text>}
+    </>
   );
 };
 
